510ddbdc3c95a80267d118ddc00e5dc87fd33c35
Serkan Özer <sekozer@Serkans-MacBook-Air.local>
2015-07-22 00:08:34 +0200

    fast dtw added

--- ScoreMatch/dtwMatch.py
+++ ScoreMatch/dtwMatch.py
@@ -1,4 +1,5 @@
 from dtw import dtw
+from fastdtw import *
 import rpy2.robjects.numpy2ri
 from rpy2.robjects.packages import importr
 rpy2.robjects.numpy2ri.activate()
@@ -17,10 +18,11 @@ def rdtwSubsequenceMatchScore(template,query):
                  open_begin=True, open_end=True,
                  step_pattern=R('asymmetric')
                  )
-	print "done"
 	return alignment.rx('normalizedDistance')[0][0]
 def rdtwGlobalMatchScore(template,query):
-	alignment = R.dtw(query, template,keep=True)
-	print "done"
-	return alignment.rx('distance')[0][0]
-#def dftSubsequenceMatchScore(sequence1,sequence2):
+	alignment = R.dtw(query, template,keep=False,keep_internals=False,distance_only=True)
+	return alignment.rx('normalizedDistance')[0][0]
+
+def fastdtwGlobalMatchScore(template,query):
+	(distance,path,) = fastdtw(query,template,dist=lambda a, b: np.linalg.norm(a-b))
+	return distance/len(path)
